92c6c59ca06296fa7c3771d1a6932960a28be4ad
Nicanor008 <nicanorkorir008@gmail.com>
2020-08-16 13:10:52 +0300

    Responsive menu button

--- src/components/common/nav/header.js
+++ src/components/common/nav/header.js
@@ -1,17 +1,65 @@
-import { Link } from "gatsby"
-import React from "react"
+import { Link, navigate } from "gatsby"
+import React, { useEffect, useState } from "react"
 import "./header.scss"
 import "../styles/_common.scss"
 import Logo from "../../../images/logo.svg"
+import Menu from "../../../images/icons/menu.svg"
+import { Loader } from "../loader"
 
-const Header = () => (
-  <header className="navHeader">
-    <div className="container navMenuWrapper">
-      <Link to="/">
-        <img src={Logo} alt="logo" className="logo" />
-      </Link>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [token, setToken] = useState("")
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    const getToken =
+      typeof window !== "undefined" && window.localStorage.getItem("token")
+    if (getToken) {
+      setToken(getToken)
+      setLoading(false)
+    } else {
+      setLoading(false)
+      setToken("")
+    }
+  })
+
+  const logoutHandler = () => {
+    localStorage.clear()
+    return navigate("/")
+  }
+  function responsiveMenu() {
+    var x = document.getElementById("myTopNav")
+    if (x.className === "myTopNav") {
+      x.className += " responsive"
+    } else {
+      x.className = "myTopNav"
+    }
+  }
+  return (
+    <header className="navHeader">
+      <div className="container navMenuWrapper navWrapper">
+        <Link to="/">
+          <img src={Logo} alt="logo" className="logo" />
+        </Link>
+        <div className="menu" id="meTest">
+          {loading ? (
+            <Loader />
+          ) : token ? (
+            <div className="myTopNav" id="myTopNav">
+              <Link to="/todo">Todo</Link>
+              <button onClick={logoutHandler}>Log out</button>
+              <span className="icon" onClick={responsiveMenu}>
+                <img src={Menu} alt="menu" />
+              </span>
+            </div>
+          ) : (
+            <button onClick={logoutHandler} className="unAuthButton">
+              Sign In|Up
+            </button>
+          )}
+        </div>
+      </div>
+    </header>
+  )
+}
 
 export default Header
