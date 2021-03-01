f8babed30fce3d64f140103c8dc3bb577ac637dc
Nicanor008 <nicanorkorir008@gmail.com>
2021-03-01 20:24:36 +0300

    Version 2 landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
-import LandingPage from "../components/LP/LandingPage"
+import Version2LandingPage from "../components/LP/LandingPage/version2LandingPage"
 
 const IndexPage = () => {
   return (
     <div>
         <Layout>
           <SEO title="Home Learning Pattern" description="LP todo Home page" />
-          <LandingPage />
+          <Version2LandingPage />
         </Layout>
     </div>
   )
