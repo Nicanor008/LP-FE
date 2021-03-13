0840c3bb04ad28d75a2da349f0230b5cc6472d46
Nicanor008 <nicanorkorir008@gmail.com>
2021-03-13 22:42:50 +0300

    Version 2 landing page

---
+++ src/components/LP/LandingPage/version2/development/development.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Features from "../../../../../images/version2/Features.svg"
+import Catalog from "../../../../../images/version2/catalog.svg"
+import "./development.scss"
+
+const Development = () => {
+    return (
+        <div className="version2Development">
+            <div className="version2Development__features">
+                <p>LP Team is working on new features and enhancements on LP Product</p>
+                <ol>
+                    <li>Analytics Enhancements</li>
+                    <li>Predict your todo activities</li>
+                    <li>Team access and collaboration on the same activities</li>
+                    <li>Organisation access</li>
+                    <li>Payment module</li>
+                </ol>
+                <img className="featuredImage" src={Features} alt="features" />
+            </div>
+            <img src={Catalog} alt="catalog" className="version2Development__catalogImage" />
+        </div>
+    )
+}
+
+export default Development
---
+++ src/components/LP/LandingPage/version2/values/value.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Value = ({ image, title, description }) => {
+    return (
+        <div className="version2values__allValues">
+            <div className="version2values__allValues-title">
+                <img className="version2values" src={image} />
+                <h3>{title}</h3>
+            </div>
+            <div className="version2values__body">
+                {description}
+            </div>
+        </div>
+    )
+}
+
+export default Value
