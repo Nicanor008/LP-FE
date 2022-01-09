b4ea44cd52df29e72b40d7235a09c4bb63aeed77
Nicanor Korir <nicanor@Nicanors-MacBook-Pro.local>
2022-01-09 10:29:11 +0300

    Remove daily statistics title text when no todos

--- src/components/LP/Todo/secondRow/analytics/analyticsChartCard.js
+++ src/components/LP/Todo/secondRow/analytics/analyticsChartCard.js
@@ -5,21 +5,23 @@ import { Loader } from "../../../../common/loader"
 function AnalyticsChartCard(props) {
   return (
     <div>
-      <Chart
-        width={"350px"}
-        height={"300px"}
-        chartType="PieChart"
-        loader={<Loader />}
-        data={[
-          ["Task", "Total Number of Tasks"],
-          ["Done", props.completedTodo.length],
-          ["Ongoing", props.ongoingTodo.length]
-        ]}
-        options={{
-          title: "Daily Statistics",
-        }}
-        rootProps={{ "data-testid": "1" }}
-      />
+      {props.completedTodo.length ? (
+        <Chart
+          width={"350px"}
+          height={"300px"}
+          chartType="PieChart"
+          loader={<Loader />}
+          data={[
+            ["Task", "Total Number of Tasks"],
+            ["Done", props.completedTodo.length],
+            ["Ongoing", props.ongoingTodo.length]
+          ]}
+          options={{
+            title: "Daily Statistics",
+          }}
+          rootProps={{ "data-testid": "1" }}
+        />
+      ): null}
     </div>
   )
 }
